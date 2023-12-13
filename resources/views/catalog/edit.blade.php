b48926c8fc64c716827f98f31eae58db4529b767
ArturoGomez <1729185@alu.murciaeduca.es>
2023-12-13 10:10:44 +0100

    Issue211

--- resources/views/catalog/edit.blade.php
+++ resources/views/catalog/edit.blade.php
@@ -31,6 +31,12 @@
 	               <input type="text" name="dominio" id="dominio" value="{{ $proyecto['dominio'] }}" class="form-control">
 	            </div>
 
+                <div class="form-group">
+	            	<label for="calificacion">Calificacion</label>
+	               <input type="number" name="calificacion" value="{{ $proyecto['calificacion'] }}" id="calificacion" min="1" max="10">
+	            </div>
+
+
 	            <div class="form-group">
 	               <label for="metadatos">Metadatos</label>
 	               <textarea name="metadatos" id="metadatos" class="form-control" value rows="3">
