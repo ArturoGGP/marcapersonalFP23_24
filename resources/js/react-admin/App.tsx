8a450eb4e26cbaa36ec45510b0153e5530c95902
ArturoGomez <1729185@alu.murciaeduca.es>
2024-01-14 18:15:15 +0100

    Tabla ciclos con React-admin

--- resources/js/react-admin/App.tsx
+++ resources/js/react-admin/App.tsx
@@ -2,6 +2,10 @@
 import { Admin, Resource, ListGuesser, EditGuesser, ShowGuesser } from 'react-admin';
 import { dataProvider } from './dataProvider';
 import { authProvider } from './authProvider';
+import CicloIcon from '@mui/icons-material/Book';
+import {
+    CicloEdit, CicloList, CicloTitle, CicloCreate, CicloShow
+} from '../Pages/Ciclos';
 
 export const App = () => (
     <Admin
@@ -11,10 +15,18 @@ export const App = () => (
         <Resource name="users" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
         <Resource name="curriculos" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
         <Resource name="proyectos" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
-        <Resource name="ciclos" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
+        <Resource name="ciclos"
+         icon={CicloIcon}
+         list={CicloList}
+         edit={CicloEdit}
+         show={CicloShow}
+         create={CicloCreate}
+         />
         <Resource name="familias_profesionales" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
         <Resource name="actividades" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
         <Resource name="reconocimientos" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
+
+
     </Admin>
 );
 
